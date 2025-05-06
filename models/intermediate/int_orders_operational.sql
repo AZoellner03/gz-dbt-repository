52ec09495493b40b5c664cd923b450a587b85b43
august.zoellner@gmail.com <august.zoellner@gmail.com>
2025-05-06 15:47:56 +0000

    added operational margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -7,7 +7,7 @@ WITH subquery AS (
     ship.shipping_fee,
     ship.ship_cost,
     ship.logcost,
-    ROUND(CAST(products.purchase_price AS FLOAT64) * sales.quantity, 2) AS purchase_cost
+    purchase_cost
   FROM 
     {{ ref('int_orders_margin') }} AS margin_table
   LEFT JOIN 
@@ -19,7 +19,7 @@ SELECT
   orders_id, 
   date_date,
   ROUND(SUM(revenue + shipping_fee), 2) AS total_revenue,
-  SUM(quantity) AS quantity, 
+  --SUM(quantity) AS quantity, 
   ROUND(SUM(purchase_cost + logcost + ship_cost), 2) AS total_cost,
   ROUND(SUM(revenue + shipping_fee) - SUM(purchase_cost + logcost + ship_cost), 2) AS operational_margin
 FROM subquery
