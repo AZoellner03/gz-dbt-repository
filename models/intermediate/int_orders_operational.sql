6b9a4b17475ea443cc679d2c5e793e21b47055cf
august.zoellner@gmail.com <august.zoellner@gmail.com>
2025-05-06 15:39:58 +0000

    added margin table

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,25 +1,26 @@
 WITH subquery AS (
   SELECT 
-    sales.orders_id,
+    margin_table.orders_id,
     date_date,
-    sales.revenue,
-    sales.quantity,
+    revenue,
+    quantity,
     ship.shipping_fee,
     ship.ship_cost,
     ship.logcost,
-    SUM(ROUND(revenue - purchase_cost, 2)) AS margin
     ROUND(CAST(products.purchase_price AS FLOAT64) * sales.quantity, 2) AS purchase_cost
   FROM 
-        {{ ref('stg_raw__sales') }} AS sales
-        LEFT JOIN {{ ref('stg_raw__ship') }} AS ship
-            USING(orders_id))), 
+    {{ ref('int_orders_margin') }} AS margin_table
+  LEFT JOIN 
+    {{ ref('stg_raw__ship') }} AS ship
+    USING(orders_id)
+)
 
 SELECT 
   orders_id, 
   date_date,
-  ROUND(revenue+shipping_fee,2) as total_revenue,
-  SUM(quantity) as quantity, 
-  ROUND(purchase_cost+logcost+ship_cost,2) as total_cost,
-  ROUND((revenue+shipping_fee)-(purchase_cost+logcost+ship_cost),2) as operational_margin
+  ROUND(SUM(revenue + shipping_fee), 2) AS total_revenue,
+  SUM(quantity) AS quantity, 
+  ROUND(SUM(purchase_cost + logcost + ship_cost), 2) AS total_cost,
+  ROUND(SUM(revenue + shipping_fee) - SUM(purchase_cost + logcost + ship_cost), 2) AS operational_margin
 FROM subquery
-group by orders_id, date_date
+GROUP BY orders_id, date_date
