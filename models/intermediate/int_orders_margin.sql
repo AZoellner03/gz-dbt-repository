294825727262da1cad1f50faab8d6173e3c04d79
august.zoellner@gmail.com <august.zoellner@gmail.com>
2025-05-07 07:11:24 +0000

    finalization of task 4.

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -3,26 +3,21 @@
 
 WITH subquery AS (
   SELECT 
-    products_id,
-    products.purchase_price,
-    sales.revenue,
-    sales.quantity,
-    sales.orders_id,
-    sales.date_date,
-    ROUND(CAST(products.purchase_price AS FLOAT64) * sales.quantity, 2) AS purchase_cost
-  FROM 
-    {{ ref('stg_raw__sales') }} AS sales
-  LEFT JOIN 
-    {{ ref('stg_raw__products') }} AS products 
-    USING(products_id)
+    orders_id,
+    date_date,
+    revenue,
+    quantity,
+    purchase_cost,
+    ROUND(revenue - purchase_cost, 2) AS margin
+  FROM {{ ref('int_sales_margin') }}
 )
 
 SELECT 
-  orders_id, 
+  orders_id,
   date_date,
   SUM(revenue) AS revenue,
-  SUM(quantity) AS quantity, 
+  SUM(quantity) AS quantity,
   SUM(purchase_cost) AS purchase_cost,
-  SUM(ROUND(revenue - purchase_cost, 2)) AS margin
+  SUM(margin) AS margin
 FROM subquery
 GROUP BY orders_id, date_date
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -8,16 +8,25 @@ WITH subquery AS (
     sales.revenue,
     sales.quantity,
     sales.orders_id,
+    sales.date_date,
     ROUND(CAST(products.purchase_price AS FLOAT64) * sales.quantity, 2) AS purchase_cost
   FROM 
-        {{ ref('stg_raw__sales') }} AS sales
-        LEFT JOIN {{ ref('stg_raw__products') }} AS products 
-            USING(products_id))
+    {{ ref('stg_raw__sales') }} AS sales
+  LEFT JOIN 
+    {{ ref('stg_raw__products') }} AS products 
+    USING(products_id)
+)
 
 SELECT 
-  products_id, 
+  products_id,
+  orders_id,
+  date_date,
+  revenue,
+  quantity,
+  purchase_cost,
   ROUND(revenue - purchase_cost, 2) AS margin
 FROM subquery
+
 
 
                 
